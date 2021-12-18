e796970ad270075c6735e2de6481986e51f29176
Phumkrit <su.phumkrit_st@tni.ac.th>
2021-12-18 09:39:06 +0700

    React router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import HomePage from './Components/pages/HomePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AboutPage from './Components/pages/AboutPage';
 
 
 function App() {
   return (
-    <>
+    <Router>
      <Navbar></Navbar>
-     <HomePage></HomePage>
-      <Footer></Footer>
+     <Switch>
+       <Route exact path='/'><HomePage/></Route>
+       <Route path='/about'><AboutPage/></Route>
+     </Switch>
+     <Footer></Footer>
       
-    </>
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Navbar,Nav,NavDropdown,Form,FormControl,Button} from "react-bootstrap";
+import { NavLink } from "react-router-dom"
 
 const Navbar_v1 = () => {
   return (
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  {/*<Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>*/}
+  <NavLink to='/' exact className="navbar-brand">React-Bootstrap</NavLink>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
+      <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+      <NavLink exact to="/about" className="nav-link" activeClassName="active">About</NavLink>
       <Nav.Link href="#link">Link</Nav.Link>
       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
