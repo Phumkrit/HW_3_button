9437cf51efded2bd8627eb0d6766730263708529
Phumkrit <su.phumkrit_st@tni.ac.th>
2021-12-18 12:58:40 +0700

    asssingment 2

--- src/Components/pages/ContactMe.js
+++ src/Components/pages/ContactMe.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Card,Button} from 'react-bootstrap'
 import { IoIosMegaphone,IoMdHappy } from "react-icons/io";
+import { BsGithub } from "react-icons/bs";
 
 const ContactMe = () => {
     return (
@@ -15,10 +16,10 @@ const ContactMe = () => {
       You can call me "Poom" that is my nickname. Have a nice Day!! <IoMdHappy/>
       <li className="mt-3">I wish I Could work about Data Sci or Data Mineing as soon as i have graduate </li>
       <li className="mt-3">I love to sleep and hang out with my friends for my freetime </li>
-      <li className="mt-3">Aiming to br rich soon!</li>
+      <li className="mt-3">Aiming to be rich soon!</li>
       <p className="mt-3">MY GITHUB</p>
     </Card.Text>
-    <Button variant="dark" href='https://github.com/Phumkrit' >Contect ME <IoIosMegaphone/></Button>
+    <Button variant="dark" href='https://github.com/Phumkrit' ><BsGithub/> Contect ME <IoIosMegaphone/></Button>
     <p className="mt-3"> Contect me : Su.phumkrit_st@tni.ac.th</p>
   </Card.Body>
 </Card>
