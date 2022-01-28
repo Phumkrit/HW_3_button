7af89e06080336a6885dc79bc738d8ceb6fbd28e
Phumkrit <su.phumkrit_st@tni.ac.th>
2022-01-28 21:57:37 +0700

    chap_6_CRUD

--- src/Components/pages/Category/EditPage.js
+++ src/Components/pages/Category/EditPage.js
@@ -1,13 +1,95 @@
 import React from 'react'
+import {Form, Button} from "react-bootstrap"
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import axios from 'axios';
+import {useParams ,useHistory} from 'react-router-dom'
 
-export default function EditPage() {
+
+const schema = yup.object({
+    name: yup.string().required('Category news cannot be null'),
+  }).required();
+
+  const EditPage = () => {
+
+    const [error, setError] = React.useState(null)
+
+    const { id } = useParams()
+
+    const history = useHistory()
+
+    const { register, handleSubmit, formState:{ errors }, setValue } = useForm({
+        resolver: yupResolver(schema)
+      });
+
+    const onSubmit = async (data) => {
+        console.log(data)
+        try{
+            const apiURL = 'https://api.codingthailand.com/api/category'
+            const resp = await axios.put(apiURL,
+                {
+                    id:id,
+                    name: data.name
+                }
+            )
+            alert('updateเสร็จสิ้น')
+            history.goBack()
+        }
+        catch(error){
+            setError(error)
+        }
+    }
+
+    React.useEffect(() => {
+        getData(id);
+    },[id])
+
+    const getData = async (id) => {
+        const resp = await axios.get('https://api.codingthailand.com/api/category/' + id);
+        setValue('name',resp.data.name)
+    };
+
+    }
+
+
+    if(error){
+        return(
+            <div className="text-center mt-5 text-danger">
+                <h4>Error from API, please try again</h4>
+                <p>{error.response.data.message}</p>
+            </div>
+        )
+    }
+
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12 mt-4">
                     <h2>EditPage</h2>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Group controlId="name">
+                            <Form.Label>Category New</Form.Label>
+                            <Form.Control type="text" name="name" ref={register} className={`form-control ${errors.name ? 'is-invalid' : ''}`} />
+                            {
+                                errors.name && (
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.name.message}
+                                    </Form.Control.Feedback>
+                                )
+                            }
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                        Update
+                        </Button>
+                    </Form>
                 </div>
             </div>            
         </div>
     )
-}
+
+    export default EditPage
+
+    
